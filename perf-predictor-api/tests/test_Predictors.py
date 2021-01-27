985904adfd565878ab39f830dad2f7dc9be4114a
IndikaKuma <ipkumarawd@yahoo.com>
2021-01-27 11:02:06 +0100

    Update test case

---
+++ perf-predictor-api/tests/test_Predictors.py
@@ -0,0 +1,14 @@
+import pandas as pd
+import utils.DTR as dtr_model
+
+
+class TestPredictors:
+
+    def test_dtr(self):
+        with open('testResources/data.json') as json_file:
+            data = pd.read_json(json_file)
+        q = data["mean_rt"].max()
+        assert 36816 == int(q)
+        dtr_model.train(data)
+        value = dtr_model.predict(data.iloc[9:10, 0:14])
+        assert value is not None
