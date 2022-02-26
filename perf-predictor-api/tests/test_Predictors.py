62f730ef925102f4be8bc6123987bd84dac11415
IndikaKuma <ipkumarawd@yahoo.com>
2022-02-26 17:42:33 +0100

    Add more tests

--- perf-predictor-api/tests/test_Predictors.py
+++ perf-predictor-api/tests/test_Predictors.py
@@ -3,6 +3,7 @@ import unittest
 import pandas as pd
 
 import mlalgo.DTR as dtr_model
+import mlalgo.MLPNN as mlp_model
 import mlalgo.RTFR as rt_model
 
 
@@ -28,7 +29,31 @@ class TestPredictors(unittest.TestCase):
     def test_rf(self):
         with open('testResources/data.json') as json_file:
             data = pd.read_json(json_file)
+        train_out = rt_model.train(data)
+        assert train_out is not None
+        value = rt_model.predict(data.iloc[9:10, 0:14])
+        assert value is not None
+
+    def test_rf_kfold(self):
+        with open('testResources/data.json') as json_file:
+            data = pd.read_json(json_file)
         train_out = rt_model.train_kfold_grid(data)
         assert train_out is not None
         value = rt_model.predict(data.iloc[9:10, 0:14])
+        assert value is not None
+
+    def test_mlp(self):
+        with open('testResources/data_nn.json') as json_file:
+            data = pd.read_json(json_file)
+        train_out = mlp_model.train(data)
+        assert train_out is not None
+        value = mlp_model.predict(data.iloc[9:10, 0:14])
+        assert value is not None
+
+    def test_mlpkfold(self):
+        with open('testResources/data_nn.json') as json_file:
+            data = pd.read_json(json_file)
+        train_out = mlp_model.train_kfold_grid(data)
+        assert train_out is not None
+        value = mlp_model.predict(data.iloc[9:10, 0:14])
         assert value is not None
